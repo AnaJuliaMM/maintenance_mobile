fd68c93c2dcc1e5529ce8f1a05851223cd60c0e5
Ana Julia M Martins <ana_juh17@hotmail.com>
2024-09-21 22:43:47 -0300

    feat: create tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,22 @@
-import { Stack } from "expo-router";
 import * as React from "react";
-import { Alert, ScrollView, View, useWindowDimensions } from "react-native";
+import {
+  Alert,
+  ScrollView,
+  Text,
+  View,
+  useWindowDimensions,
+} from "react-native";
+
+// External Package imports
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { router } from "expo-router";
 
 // Custom components imports
 import CustomTable from "~/components/custom/CustomTable";
 
 // Mock data imports
 import { MACHINES } from "~/lib/mock_data";
-
+import { PiWashingMachine } from "react-icons/pi";
 
 export default function Example() {
   return (
@@ -20,6 +29,9 @@ export default function Example() {
         rows={MACHINES}
         columns={["Nome", "Tipo", "Localização"]}
         min_column_widths={[20, 20, 20]}
+        onPressRow={() => {
+          router.navigate("/machineModal");
+        }}
       />
     </ScrollView>
   );
