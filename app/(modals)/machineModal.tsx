98f967a23e623cef01654b567c9f39a914fc085b
Ana Julia M Martins <ana_juh17@hotmail.com>
2024-09-21 22:44:19 -0300

    feat: detail machine modal

---
+++ app/(modals)/machineModal.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function MachineModal() {
+  return (
+    <View>
+      <Text>machineModal</Text>
+    </View>
+  )
+}
