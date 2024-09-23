637581ef439cf7517dc152db74744263057c0ffd
Ana Julia M Martins <ana_juh17@hotmail.com>
2024-09-22 23:10:20 -0300

    feat: add table as index maintenance pages

--- lib/types.ts
+++ lib/types.ts
@@ -1,6 +1,20 @@
 export type MachineType = {
   name: string;
   type: string;
-  location: string;
+  model: string;
+  manufactureDate: string;
   serialNumber: string;
+  location: string;
+  imagesUrl: string[];
+};
+
+export type MaintenanceType = {
+  code: string;
+  title: string;
+  description: string;
+  status: string;
+  type: "preventiva" | "corretiva" | "preditiva";
+  requisitionDate: string;
+  priority: string;
+  responsableTeam: string
 };
