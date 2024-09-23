2ae9324ef1c8ac4b87ae246510497f7e68d7056f
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-09-23 18:39:02 -0300

    feat: add maintenances history

--- app/machine/[id].tsx
+++ app/machine/[id].tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
-import { View, Image } from "react-native";
+import { View, Image, ScrollView } from "react-native";
 
 // React Native Reusables components imports
 import { Text } from "~/components/ui/text";
@@ -11,9 +11,12 @@ import { Button } from "~/components/ui/button";
 
 // Custom components imports
 import CustomCard from "~/components/custom/CustomCard";
+import CustomTable from "~/components/custom/CustomTable";
 
 // Mock data
 import { MACHINES } from "~/lib/mock_data";
+import { MAINTENANCES } from "~/lib/mock_data";
+import { INVENTORY } from "~/lib/mock_data";
 
 /**
  * Tela que exibe os detalhes de uma máquina com base no ID fornecido na URL.
@@ -25,8 +28,7 @@ import { MACHINES } from "~/lib/mock_data";
  */
 export default function DetalhesMaquina() {
   const [checked, setChecked] = React.useState(false);
-  const [value, setValue] = React.useState('');
-
+  const [value, setValue] = React.useState("");
 
   // Obtém o ID da máquina a partir dos parâmetros da rota.
   const { id } = useLocalSearchParams();
@@ -35,7 +37,12 @@ export default function DetalhesMaquina() {
   const machine = MACHINES.filter((item) => item.serialNumber == id)[0];
 
   return (
-    <View className="p-2 ">
+    <ScrollView
+      alwaysBounceVertical={true}
+      bounces={false}
+      showsHorizontalScrollIndicator={false}
+      className="p-2 "
+    >
       <View className="flex-row items-center gap-2 justify-between w-full p-4 pb-8">
         <Label
           nativeID="airplane-mode"
@@ -60,19 +67,29 @@ export default function DetalhesMaquina() {
         <Text>Data de fabricação: {machine.manufactureDate}</Text>
         <Text>N° de Série: {machine.serialNumber}</Text>
       </CustomCard>
-      
-      <View className="flex gap-8 pt-8">
-        <Text className="font-bold text-xl">Comentários</Text>
-        <Textarea
-        placeholder='Adicione comentários'
-        value={value}
-        onChangeText={setValue}
-        aria-labelledby='textareaLabel'
-      />
-      <Button className="bg-blue-500">
-        <Text>Enviar</Text>
-      </Button>
+
+      <View className="flex flex-1 flex-col p-2 pt-8">
+        <Text className="text-2xl font-bold">Manutenções Recentes</Text>
+        <CustomTable
+            rows={MAINTENANCES}
+            columns={["Nome", "Data", "Status"]}
+            keys={["code", "title", "requisitionDate", "status"]}
+            min_column_widths={[40, 10, 10]}
+          />
       </View>
-    </View>
+
+      <View className="flex flex-1 gap-8 pt-8">
+        <Text className="font-bold text-2xl">Comentários</Text>
+        <Textarea
+          placeholder="Adicione comentários"
+          value={value}
+          onChangeText={setValue}
+          aria-labelledby="textareaLabel"
+        />
+        <Button className="bg-blue-500">
+          <Text>Enviar</Text>
+        </Button>
+      </View>
+    </ScrollView>
   );
 }
